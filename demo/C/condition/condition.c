41330af44ba54b4ba552a0a1aa2c7aae59865dc7
askYangChuan <641617487@qq.com>
2018-11-30 11:42:20 +0800

    C版本的condition

---
+++ demo/C/condition/condition.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <errno.h>
+
+#include "condition.h"
+
+void condition_free(condition_t *t)
+{
+	if(t) {
+		if(t->lock_free) {
+			pthread_mutex_destroy(t->lock_);
+			free(t->lock_);
+		}
+		pthread_cond_destroy(&t->pcond_);
+		free(t);
+	}
+}
+
+condition_t *condition_init(pthread_mutex_t *lock)
+{
+	condition_t *t = calloc(1, sizeof(condition_t));
+	if(t == NULL)
+		return NULL;
+
+	if(lock) {
+		t->lock_free = 0;
+		t->lock_ = lock;
+	}else {
+		t->lock_ = calloc(1, sizeof(pthread_mutex_t));
+		if(t->lock_ == NULL) {
+			free(t);
+			return NULL;
+		}
+		pthread_mutex_init(t->lock_, NULL);
+		t->lock_free = 1;
+	}
+	
+	pthread_cond_init(&t->pcond_, NULL);
+
+	return t;
+}
+
+pthread_mutex_t *condition_getLock(condition_t *t)
+{
+	return t->lock_;
+}
+
+void condition_wait(condition_t *t)
+{
+	pthread_cond_wait(&t->pcond_, t->lock_);
+}
+
+int condition_waitForSeconds(condition_t *t, double seconds)
+{
+	struct timespec abstime;
+	// FIXME: use CLOCK_MONOTONIC or CLOCK_MONOTONIC_RAW to prevent time rewind.
+	clock_gettime(CLOCK_REALTIME, &abstime);
+
+	const int64_t kNanoSecondsPerSecond = 1000000000;
+	int64_t nanoseconds = seconds * kNanoSecondsPerSecond;
+
+	abstime.tv_sec += (abstime.tv_nsec + nanoseconds) / kNanoSecondsPerSecond;
+	abstime.tv_nsec = (abstime.tv_nsec + nanoseconds) % kNanoSecondsPerSecond;
+
+	return ETIMEDOUT == pthread_cond_timedwait(&t->pcond_, t->lock_, &abstime);
+}
+
+void condition_notify(condition_t *t)
+{
+	pthread_cond_signal(&t->pcond_);
+}
+
+void condition_notifyAll(condition_t *t)
+{
+	pthread_cond_broadcast(&t->pcond_);
+}
+
---
+++ demo/C/condition/main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <pthread.h>
+#include "condition.h"
+#include <unistd.h>
+
+condition_t *t1;
+
+#define BUF_MAX 30
+
+int buf[BUF_MAX] = {0};
+
+int head = 0;
+int tail = 0;
+
+
+void *consumer(void *p)
+{
+	int timeout;
+	while(1) {
+		pthread_mutex_lock(condition_getLock(t1));
+		while(head == tail) {
+			//condition_wait(t1);
+			timeout = condition_waitForSeconds(t1, 1);
+			if(timeout) {
+				printf("I'm timeout\n");
+			}
+		}
+		printf("buf[%d]: %d\n", head, buf[head]);
+		head++;
+		pthread_mutex_unlock(condition_getLock(t1));
+	}
+	return NULL;
+}
+
+void *producer(void *p)
+{
+	int run = 1;
+	while(run) {
+		pthread_mutex_lock(condition_getLock(t1));
+		buf[tail] = tail;
+		tail++;
+		if(tail == BUF_MAX) {
+			run = 0;
+		}
+		condition_notify(t1);
+		pthread_mutex_unlock(condition_getLock(t1));
+		usleep(2000000);
+	}
+	printf("producer over\n");
+	return NULL;
+}
+
+
+int main()
+{
+	pthread_t pid1, pid2;
+
+	t1 = condition_init(NULL);
+
+	pthread_create(&pid1, NULL, producer, NULL);
+	pthread_create(&pid2, NULL, consumer, NULL);
+
+	
+	pthread_join(pid1, NULL);
+	pthread_join(pid2, NULL);
+
+	condition_free(t1);
+	
+	return 0;
+}
