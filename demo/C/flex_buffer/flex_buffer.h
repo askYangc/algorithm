0858c43ecb90f73160f6dd2cd14fa492423dc8df
yangchuan <641617487@qq.com>
2020-07-27 14:17:19 +0800

    增加了一个集中管理的结构体，用于异步线程处理

--- demo/C/flex_buffer/flex_buffer.h
+++ demo/C/flex_buffer/flex_buffer.h
@@ -26,6 +26,12 @@ typedef struct {
     int size;
 }flex_buffer_t;
 
+typedef struct {
+	flex_buffer_t *buffers_;
+	int cout;
+	int max;
+}alignflexbuffer_t;
+
 
 static inline int readableBytes(flex_buffer_t *b)
 { 
@@ -51,6 +57,7 @@ static inline const char* flex_beginWrite(flex_buffer_t *b)
 static inline void flex_retrieveAll(flex_buffer_t *b)
 {
   b->readerIndex_ = b->writerIndex_ = 0;
+  memset(b->_buffer, 0, b->size);
 }
 
 // retrieve returns void, to prevent
@@ -61,6 +68,7 @@ static inline void flex_retrieve(flex_buffer_t *b, int len)
   assert(len <= readableBytes(b));
   if (len < readableBytes(b))
   {
+    memset(&b->_buffer[b->readerIndex_], 0, len);
     b->readerIndex_ += len;
   }
   else
@@ -99,6 +107,7 @@ static inline void flex_makeSpace(flex_buffer_t *b, size_t len)
         char *tmp = (char*)realloc(b->_buffer, b->writerIndex_ + len);
         assert(tmp != NULL);
         b->_buffer = tmp;
+        memset(&b->_buffer[b->size], 0, b->writerIndex_ + len - b->size);
         b->size = b->writerIndex_ + len;
     }
 }
@@ -127,6 +136,35 @@ static inline int flex_append(flex_buffer_t *b, char *data, int len)
     return len;
 }
 
+
+
+static inline void flex_buffer_clean(flex_buffer_t *b, int isfree)
+{
+    if(b) {
+        b->readerIndex_ = b->writerIndex_ = 0;
+        b->size = 0;
+        if(isfree && b->_buffer) {
+            free(b->_buffer);
+        }
+        b->_buffer = NULL;
+    }
+}
+
+static inline void flex_buffer_move(flex_buffer_t *dst, flex_buffer_t *src, int clean)
+{
+    if(clean) {
+        flex_buffer_clean(dst, 1);
+    }
+    memcpy(dst, src, sizeof(flex_buffer_t));
+    flex_buffer_clean(src, 0);
+}
+
+static inline int flex_buffer_usable(flex_buffer_t *b)
+{
+    return b->_buffer?1:0;
+}
+
+
 static inline void flex_buffer_free(flex_buffer_t *b)
 {
     if(b) {
@@ -135,6 +173,13 @@ static inline void flex_buffer_free(flex_buffer_t *b)
         free(b);
     }
 }
+
+#define flex_buffer_safe_free(b) do{\
+    if(b){\
+        flex_buffer_free(b);\
+        b = NULL;\
+    }\
+}while(0)
 
 static inline flex_buffer_t *flex_buffer_init(int size)
 {
@@ -151,6 +196,80 @@ static inline flex_buffer_t *flex_buffer_init(int size)
     
     return b;
 }
+
+
+static inline int flex_buffer_recalloc(flex_buffer_t *b, int size)
+{
+    if(b) {
+        if(b->_buffer) {
+            free(b->_buffer);
+        }
+        b->_buffer = (char*)calloc(1, size);
+        if(!b->_buffer) {
+            free(b);
+            return -1;
+        }
+        b->readerIndex_ = b->writerIndex_ = 0;
+        b->size = size;
+    }
+    
+    return 0;
+}
+
+
+static inline void alignflexbuffer_add_tail(alignflexbuffer_t *t, flex_buffer_t *buffer)
+{
+	if(t->cout >= t->max)
+		return ;
+	t->buffers_[t->cout++] = *buffer;
+    flex_buffer_clean(buffer, 0);
+}
+
+static inline void alignflexbuffer_erase(alignflexbuffer_t *t, int begin, int end)
+{
+	int i = 0; 
+	for(i = begin; i < end; i++) {
+		flex_buffer_clean(&t->buffers_[i], 1);
+	}
+	if(begin >= 0) {
+		t->cout = begin;
+	}
+}
+
+static inline void alignflexbuffer_move(alignflexbuffer_t *dst, alignflexbuffer_t *src)
+{
+	int max = (dst->max>src->cout)?src->cout:dst->max;
+
+	alignflexbuffer_erase(dst, 0, dst->cout);
+	memcpy(dst->buffers_, src->buffers_, max*sizeof(flex_buffer_t));
+	memset(src->buffers_, 0, max*sizeof(flex_buffer_t));
+	dst->cout = max;
+	src->cout = 0;	
+}
+
+static inline void alignflexbuffer_free(alignflexbuffer_t *t)
+{
+	if(t) {
+		if(t->buffers_) {
+			alignflexbuffer_erase(t, 0, t->cout);
+			free(t->buffers_);
+			t->buffers_ = NULL;
+		}
+		t->cout = t->max = 0;
+	}
+}
+
+static inline void alignflexbuffer_clear(alignflexbuffer_t *t)
+{
+	alignflexbuffer_erase(t, 0, t->cout);
+}
+
+static inline void alignflexbuffer_init(alignflexbuffer_t *t, int max)
+{
+	t->buffers_ = (flex_buffer_t*)calloc(1, sizeof(flex_buffer_t)*max);
+	t->max = max;
+}
+
 
 
 #endif
